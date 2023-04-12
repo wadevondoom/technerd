e1407631fefaaff2ebcdb803d5b3887a53f00de4
wadevondoom <wade.neuwirth@gmail.com>
2023-04-11 20:56:33 -0700

    Trying a game out.

--- static/er.js
+++ static/er.js
@@ -1,85 +1,125 @@
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
+// game.js
 
-let player = {
-    x: 50,
-    y: 200,
-    width: 50,
-    height: 50,
-    speed: 5,
-    jumping: false,
-    jumpHeight: 100,
-    jumpCount: 0,
-    jumpLimit: 2
+const game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameCanvas', null);
+
+const StartScreen = {
+    preload: function () {
+        // Load button image or any other assets needed for the start screen
+        game.load.image('startButton', 'assets/startButton.png');
+    },
+    create: function () {
+        // Add a start button
+        const startButton = game.add.button(game.world.centerX, game.world.centerY, 'startButton', this.startGame, this);
+        startButton.anchor.set(0.5);
+    },
+    startGame: function () {
+        game.state.start('MainGame');
+    }
 };
 
-function drawPlayer() {
-    ctx.fillStyle = "red";
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+const MainGame = {
+    preload: preload,
+    create: create,
+    update: update,
+};
+
+game.state.add('StartScreen', StartScreen);
+game.state.add('MainGame', MainGame);
+game.state.start('StartScreen');
+
+
+let player;
+let cursors;
+let ground;
+let background;
+let gameOverText;
+let restartButton;
+
+function preload() {
+    game.load.image('player1', '/static/assets/p1.png');
+    game.load.image('player2', '/static/assets/p2.png');
+    game.load.image('ground', '/static/assets/ground.png');
+    game.load.image('background', '/static/assets/background.png');
 }
 
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = 1000;
+
+    // Set up the background
+    background = game.add.tileSprite(0, 0, game.width, game.height, 'background');
+    background.fixedToCamera = true;
+
+    // Set up the ground
+    ground = game.add.tileSprite(0, game.height - 173, game.width, 173, 'ground');
+    game.physics.arcade.enable(ground);
+    ground.body.immovable = true;
+    ground.body.allowGravity = false;
+
+    // Set up the player
+    player = game.add.sprite(100, game.world.height - 300, 'player1');
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    player.anchor.setTo(0.5, 0.5);
+
+    // Create a simple two-frame walking animation
+    player.animations.add('walk', ['player1', 'player2'], 10, true);
+    player.animations.play('walk');
+
+    // Set up the platforms group
+    platforms = game.add.group();
+    platforms.enableBody = true;
+
+    // Create custom platforms
+    createPlatform(400, 400, 300, 20); // Example platform
+
+    // Set up input for player movement
+    cursors = game.input.keyboard.createCursorKeys();
+}
+
+function createPlatform(x, y, width, height) {
+    const platform = game.add.graphics(x, y);
+    platform.beginFill(0xffffff);
+    platform.lineStyle(2, 0xffffff, 1);
+    platform.drawRoundedRect(0, 0, width, height, 10);
+    platform.endFill();
+
+    game.physics.arcade.enable(platform);
+    platform.body.immovable = true;
+    platform.body.allowGravity = false;
+    platforms.add(platform);
+}
+
+function gameOver() {
+    player.kill(); // Hide the player
+    gameOverText = game.add.text(game.world.centerX, game.world.centerY - 100, 'Game Over', { font: '48px Arial', fill: '#ff0000' });
+    gameOverText.anchor.set(0.5);
+
+    // Add a restart button
+    restartButton = game.add.button(game.world.centerX, game.world.centerY, 'startButton', restartGame, this);
+    restartButton.anchor.set(0.5);
+}
+
+function restartGame() {
+    // Clean up game over elements
+    gameOverText.destroy();
+    restartButton.destroy();
+
+    // Reset the game state
+    game.state.restart();
+}
 function update() {
-    // Move player left or right
-    if (keys.left) {
-        player.x -= player.speed;
-    } else if (keys.right) {
-        player.x += player.speed;
-    }
+    game.physics.arcade.collide(player, ground, gameOver);
+    game.physics.arcade.collide(player, platforms);
 
-    // Jump logic
-    if (keys.up && !player.jumping && player.jumpCount < player.jumpLimit) {
-        player.jumping = true;
-        player.jumpCount++;
-    }
-
-    if (player.jumping) {
-        player.y -= player.jumpHeight / 10;
-        player.jumpHeight--;
-
-        if (player.jumpHeight < 0) {
-            player.jumping = false;
-            player.jumpHeight = 100;
-        }
+    // Player movement
+    player.body.velocity.x = 200; // Constant horizontal speed
+    if (cursors.up.isDown && (player.body.touching.down || player.body.onFloor())) {
+        player.body.velocity.y = -500; // Adjust jump height as needed
     }
 }
 
-function draw() {
-    // Clear canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw player
-    drawPlayer();
-}
 
-let keys = {
-    left: false,
-    right: false,
-    up: false
-};
 
-document.addEventListener("keydown", function (event) {
-    if (event.keyCode === 37) {
-        keys.left = true;
-    } else if (event.keyCode === 39) {
-        keys.right = true;
-    } else if (event.keyCode === 38) {
-        keys.up = true;
-    }
-});
 
-document.addEventListener("keyup", function (event) {
-    if (event.keyCode === 37) {
-        keys.left = false;
-    } else if (event.keyCode === 39) {
-        keys.right = false;
-    } else if (event.keyCode === 38) {
-        keys.up = false;
-    }
-});
-
-function gameLoop() {
-    update();
-    draw();
-}
-
-setInterval(gameLoop, 30);
