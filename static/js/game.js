0779c25cdcecd295e4c98c92053a7410d08610ea
wadevondoom <wade.neuwirth@gmail.com>
2023-04-12 14:38:14 -0700

    MVP game

--- static/js/game.js
+++ static/js/game.js
@@ -19,32 +19,6 @@ function preload() {
     game.load.image('startButton', '/static/assets/startButton.png');
 }
 
-function create() {
-    // Display the background image
-    background = game.add.tileSprite(0, 0, game.width, game.height, 'background');
-    background.fixedToCamera = true;
-
-    // Display the ground sprite
-    ground = game.add.tileSprite(0, game.height - 173, game.width, 173, 'ground');
-    game.physics.arcade.enable(ground);
-    ground.body.immovable = true;
-    ground.body.allowGravity = false;
-
-    // Display the player sprite
-    player = game.add.sprite(100, game.world.height - 300, 'player1');
-    game.physics.arcade.enable(player);
-    player.body.collideWorldBounds = true;
-    player.anchor.setTo(0.5, 0.5);
-
-    // Create a simple two-frame walking animation
-    player.animations.add('walk', ['player1', 'player2'], 10, true);
-    player.animations.play('walk');
-
-    // Display any platforms that you have defined
-    platforms.forEach(function (platform) {
-        platform.visible = true;
-    });
-}
 
 function createPlatform(x, y, width, height) {
     const platform = game.add.graphics(x, y);
@@ -95,15 +69,6 @@ function create() {
     // Set up the ground
     ground = game.add.tileSprite(0, game.height - 173, game.width, 173, 'ground');
     game.physics.arcade.enable(ground);
-    ground.body.immovable
-
-    // Set up the background
-    background = game.add.tileSprite(0, 0, game.width, game.height, 'background');
-    background.fixedToCamera = true;
-
-    // Set up the ground
-    ground = game.add.tileSprite(0, game.height - 173, game.width, 173, 'ground');
-    game.physics.arcade.enable(ground);
     ground.body.immovable = true;
     ground.body.allowGravity = false;
 
@@ -120,6 +85,7 @@ function create() {
     // Set up the platforms group
     platforms = game.add.group();
     platforms.enableBody = true;
+    game.physics.arcade.enable(platforms); // Add this line
 
     // Create custom platforms
     createPlatform(400, 400, 300, 20); // Example platform
