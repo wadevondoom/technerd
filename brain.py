0bdab2986ca68218f86bfcce71ca7207cc6f70ac
wadevondoom <wade.neuwirth@gmail.com>
2023-04-13 15:14:44 -0700

    Wasn't saving the image properly

--- brain.py
+++ brain.py
@@ -1,6 +1,8 @@
 import openai
+import requests
 from os import environ as env
 from dotenv import find_dotenv, load_dotenv
+from helpers import save_dalle_image
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -8,12 +10,15 @@ if ENV_FILE:
 
 openai.api_key = env.get("OPENAI_SECRET")
 
+
 class Brain:
     def __init__(self, role, prompt):
         self.role = "user"
         self.prompt = prompt
         self.engine = "davinci-003"
-        self.prompt_img_helper_text = "A close up, studio photographic portrait of the following subject:"
+        self.prompt_img_helper_text = (
+            "A close up, studio photographic portrait of the following subject:"
+        )
         self.prompt_helper_text = "Format your response in HTML. \
             Do not use H1 or H2. \
             Take persona of knowledge expert and write detailed article on the following subject:"
@@ -33,13 +38,17 @@ class Brain:
         generated_text = response.choices[0].text.strip()
         print("Text generated: " + generated_text)
         return generated_text
-    
+
     def get_dalle_image(self):
         prompt = self.prompt_img_helper_text + self.prompt
         print("Generating image...")
-        response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="1024x1024"
-        )
-        image_url = response['data'][0]['url']
+        response = openai.Image.create(prompt=prompt, n=1, size="1024x1024")
+        image_url = response["data"][0]["url"]
+
+        # Download the image from the OpenAI API URL
+        image_response = requests.get(image_url)
+
+        # Save the image locally and get the local URL
+        local_image_url = save_dalle_image(image_response.content)
+
+        return local_image_url
