7526902b63aeddda032fd0dafc28b5352d9cbe91
wadevondoom <wade.neuwirth@gmail.com>
2023-04-13 23:57:03 -0700

    Indentation error

--- brain.py
+++ brain.py
@@ -22,27 +22,27 @@ class Brain:
             Take persona of knowledge expert and write detailed article on the following subject:"
 
 
-def get_response(self):
-    # Use the OpenAI API to generate text based on the input field
-    prompt = self.prompt_helper_text + self.prompt
-    print("Generating text...")
+    def get_response(self):
+        # Use the OpenAI API to generate text based on the input field
+        prompt = self.prompt_helper_text + self.prompt
+        print("Generating text...")
 
-    # Calculate tokens in the prompt and adjust max_tokens
-    prompt_tokens = len(openai.api.encoders.get_encoder("bpe").encode(prompt))
-    max_context_length = 4096
-    max_tokens = max_context_length - prompt_tokens
+        # Calculate tokens in the prompt and adjust max_tokens
+        prompt_tokens = len(openai.api.encoders.get_encoder("bpe").encode(prompt))
+        max_context_length = 4096
+        max_tokens = max_context_length - prompt_tokens
 
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    generated_text = response.choices[0].text.strip()
-    print("Text generated: " + generated_text)
-    return generated_text
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+        generated_text = response.choices[0].text.strip()
+        print("Text generated: " + generated_text)
+        return generated_text
 
     def get_dalle_image(self):
         prompt = self.prompt_img_helper_text + self.prompt
