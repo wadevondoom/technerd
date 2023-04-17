5701c30754fd77692c0d52a7c1ac13797cc3b9aa
wadevondoom <wade.neuwirth@gmail.com>
2023-04-16 20:13:18 -0700

    StartScene and bg

--- static/js/carwarring.js
+++ static/js/carwarring.js
@@ -1,12 +1,10 @@
 // velocityFromRotation() can be called like a plain function.
 const VelocityFromRotation = Phaser.Physics.Arcade.ArcadePhysics.prototype.velocityFromRotation;
 
-class Racecar extends Phaser.Physics.Arcade.Image
-{
+class Racecar extends Phaser.Physics.Arcade.Image {
     throttle = 0;
 
-    configure ()
-    {
+    configure() {
         this.angle = -90;
 
         this.body.angularDrag = 120;
@@ -15,31 +13,25 @@ class Racecar extends Phaser.Physics.Arcade.Image
         this.body.setSize(64, 64, true);
     }
 
-    update (delta, cursorKeys)
-    {
+    update(delta, cursorKeys) {
         const { left, right, up, down } = cursorKeys;
 
-        if (up.isDown)
-        {
+        if (up.isDown) {
             this.throttle += 0.6 * delta;
         }
-        else if (down.isDown)
-        {
+        else if (down.isDown) {
             this.throttle -= 1.5 * delta;
         }
 
         this.throttle = Phaser.Math.Clamp(this.throttle, -64, 1024);
 
-        if (left.isDown)
-        {
+        if (left.isDown) {
             this.body.setAngularAcceleration(-360);
         }
-        else if (right.isDown)
-        {
+        else if (right.isDown) {
             this.body.setAngularAcceleration(360);
         }
-        else
-        {
+        else {
             this.body.setAngularAcceleration(0);
         }
 
@@ -49,16 +41,13 @@ class Racecar extends Phaser.Physics.Arcade.Image
     }
 }
 
-class MainScene extends Phaser.Scene
-{
-    preload ()
-    {
+class MainScene extends Phaser.Scene {
+    preload() {
         this.load.image('ground', '/static/assets/carwars/sprites/ground.jpg');
         this.load.image('car', '/static/assets/carwars/sprites/two-way.png');
     }
 
-    create ()
-    {
+    create() {
         this.ground = this.add.tileSprite(512, 512, 1024, 1024, 'ground').setScrollFactor(0, 0);
 
         this.car = new Racecar(this, 256, 512, 'car');
@@ -71,8 +60,7 @@ class MainScene extends Phaser.Scene
         this.cameras.main.startFollow(this.car);
     }
 
-    update (time, delta)
-    {
+    update(time, delta) {
         const { scrollX, scrollY } = this.cameras.main;
 
         this.ground.setTilePosition(scrollX, scrollY);
@@ -81,7 +69,22 @@ class MainScene extends Phaser.Scene
     }
 }
 
+class StartScene extends Phaser.Scene {
+    preload() {
+        this.load.image('startScreen', '/static/assets/carwars/sprites/startScreen.jpg');
+    }
 
+    create() {
+        this.add.image(400, 300, 'startScreen');
+        this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+
+    update() {
+        if (this.spacebar.isDown) {
+            this.scene.start('MainScene');
+        }
+    }
+}
 
 const config = {
     type: Phaser.AUTO,
@@ -100,7 +103,7 @@ const config = {
         parent: 'gameCanvas',
         fullscreenTarget: 'gameCanvas',
     },
-    scene: [MainScene],
+    scene: [StartScene],
 };
 
 const game = new Phaser.Game(config);
