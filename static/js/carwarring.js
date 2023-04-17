e36658ec0932e00b2c831aa6966e7505779d6085
wadevondoom <wade.neuwirth@gmail.com>
2023-04-16 19:52:41 -0700

    Restart

--- static/js/carwarring.js
+++ static/js/carwarring.js
@@ -1,37 +1,45 @@
 // velocityFromRotation() can be called like a plain function.
 const VelocityFromRotation = Phaser.Physics.Arcade.ArcadePhysics.prototype.velocityFromRotation;
 
-class Racecar extends Phaser.Physics.Arcade.Image {
+class Racecar extends Phaser.Physics.Arcade.Image
+{
     throttle = 0;
 
-    configure() {
+    configure ()
+    {
         this.angle = -90;
 
         this.body.angularDrag = 120;
-        this.body.maxSpeed = 768;
+        this.body.maxSpeed = 1024;
 
         this.body.setSize(64, 64, true);
     }
 
-    update(delta, cursorKeys) {
+    update (delta, cursorKeys)
+    {
         const { left, right, up, down } = cursorKeys;
 
-        if (up.isDown) {
+        if (up.isDown)
+        {
             this.throttle += 0.5 * delta;
         }
-        else if (down.isDown) {
-            this.throttle -= 1.5 * delta;
+        else if (down.isDown)
+        {
+            this.throttle -= 0.5 * delta;
         }
 
         this.throttle = Phaser.Math.Clamp(this.throttle, -64, 1024);
 
-        if (left.isDown) {
+        if (left.isDown)
+        {
             this.body.setAngularAcceleration(-360);
         }
-        else if (right.isDown) {
+        else if (right.isDown)
+        {
             this.body.setAngularAcceleration(360);
         }
-        else {
+        else
+        {
             this.body.setAngularAcceleration(0);
         }
 
@@ -41,16 +49,17 @@ class Racecar extends Phaser.Physics.Arcade.Image {
     }
 }
 
-class MainScene extends Phaser.Scene {
-    preload() {
-        this.load.image('ground', '/static/assets/carwars/sprites/ground.jpg');
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+        this.load.image('ground', '/static/assets/carwars/sprites/ground.png');
         this.load.image('car', '/static/assets/carwars/sprites/two-way.png');
     }
 
-    create() {
-        console.log("MainScene create");
-
-        this.ground = this.add.tileSprite(0, 0, config.width, config.height, 'ground').setOrigin(0, 0).setScrollFactor(0);
+    create ()
+    {
+        this.ground = this.add.tileSprite(256, 256, 512, 512, 'ground').setScrollFactor(0, 0);
 
         this.car = new Racecar(this, 256, 512, 'car');
         this.add.existing(this.car);
@@ -59,28 +68,11 @@ class MainScene extends Phaser.Scene {
 
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-        this.cameras.main.startFollow(this.car, true, 0.05, 0.05);
-
-        this.input.keyboard.on('keydown-F', () => {
-            this.toggleFullscreen();
-        });
+        this.cameras.main.startFollow(this.car);
     }
 
-    shutdown() {
-        this.input.keyboard.off('keydown-F', this.toggleFullscreen, this);
-    }
-
-    toggleFullscreen() {
-        if (this.scale.isFullscreen) {
-            this.scale.stopFullscreen();
-        } else {
-            this.scale.startFullscreen();
-        }
-    }
-
-    update(time, delta) {
-        console.log("MainScene update");
-
+    update (time, delta)
+    {
         const { scrollX, scrollY } = this.cameras.main;
 
         this.ground.setTilePosition(scrollX, scrollY);
@@ -89,54 +81,6 @@ class MainScene extends Phaser.Scene {
     }
 }
 
-class GameOverScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'GameOverScene' });
-    }
-
-    create() {
-        console.log("GameOverScene create");
-
-        this.add.text(400, 300, 'Game Over\nPress SPACE to restart', { fontSize: '32px', color: '#ffffff', align: 'center' }).setOrigin(0.5);
-
-        this.input.keyboard.on('keydown-SPACE', () => {
-            this.scene.start('MainScene');
-        });
-    }
-}
-
-class StartScene extends Phaser.Scene {
-    constructor() {
-        super({ key: 'StartScene' });
-    }
-
-    create() {
-        console.log("StartScene create");
-
-        this.add.text(400, 300, 'Press SPACE to start', { fontSize: '32px', color: '#ffffff' }).setOrigin(0.5);
-
-        this.startingMainScene = false;
-
-        this.input.keyboard.on('keydown-SPACE', () => {
-            if (!this.startingMainScene) {
-                this.startingMainScene = true;
-                this.scene.start('MainScene');
-            }
-        });
-
-        this.input.keyboard.on('keydown-F', () => {
-            this.toggleFullscreen();
-        });
-    }
-
-    toggleFullscreen() {
-        if (this.scale.isFullscreen) {
-            this.scale.stopFullscreen();
-        } else {
-            this.scale.startFullscreen();
-        }
-    }
-}
 
 
 const config = {
@@ -156,7 +100,7 @@ const config = {
         parent: 'gameCanvas',
         fullscreenTarget: 'gameCanvas',
     },
-    scene: [StartScene, MainScene, GameOverScene],
+    scene: [MainScene],
 };
 
 const game = new Phaser.Game(config);
