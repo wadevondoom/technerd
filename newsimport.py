b534eea1439d7e140a886d8fa415db2a07201777
wadevondoom <wade.neuwirth@gmail.com>
2023-04-18 11:35:00 -0700

    Added cleanup to newimport, hopefully fixed like button

--- newsimport.py
+++ newsimport.py
@@ -45,5 +45,19 @@ def get_top_news(category, num_articles):
                     "category": category,
                 }
             )
+        # Clean up so no borken images
+        articles_collection = db["articles"]
+        # Find documents with a null image_url and update them
+        articles_to_update = articles_collection.find({"image_url": {"$eq": None}})
+        updated_count = 0
+        for article in articles_to_update:
+            article_id = article["_id"]
+            result = articles_collection.update_one(
+                {"_id": article_id},
+                {"$set": {"image_url": "/static/img/placeholder.jpg"}},
+            )
+            updated_count += result.modified_count
+
+        print(f"{updated_count} documents updated.")
     else:
         print(f"Error: {response.status_code}")
